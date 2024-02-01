841c9a944c3e823b78540954e046e71bf366244a
danielecapano <daniele.capano@gmail.com>
2024-02-01 19:19:13 +0100

    ADD - style page register

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -13,60 +13,88 @@
                             @csrf
     
                             <div class="input">
+                               
+                                <input id="business_name" type="text" class="form-control @error('business_name') is-invalid @enderror" name="business_name" value="{{ old('business_name') }}" required autocomplete="business_name" autofocus>
+
+                                @error('business_name')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                                @enderror
+
                                 <label for="business_name" class="label-input">{{ __('Nome attività') }}</label>
-    
-                                <div class="">
-                                    <input id="business_name" type="text" class="form-control @error('business_name') is-invalid @enderror" name="business_name" value="{{ old('business_name') }}" required autocomplete="business_name" autofocus>
-    
-                                    @error('business_name')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                    @enderror
-                                </div>
+                              
                             </div>
     
-                            <div class="mb-4 row">
-                                <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('Indirizzo email') }}</label>
-    
-                                <div class="col-md-6">
-                                    <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
-    
-                                    @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                    @enderror
-                                </div>
+                            <div class="input">
+                                
+                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
+
+                                @error('email')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                                @enderror
+
+                                <label for="email" class="label-input">{{ __('Indirizzo email') }}</label>
+                                
                             </div>
     
-                            <div class="mb-4 row">
-                                <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
-    
-                                <div class="col-md-6">
-                                    <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
-    
-                                    @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                    @enderror
-                                </div>
+                            <div class="input">
+                                
+                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
+
+                                @error('password')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                                @enderror
+
+                                <label for="password" class="label-input">{{ __('Password') }}</label>
+                                
                             </div>
     
-                            <div class="mb-4 row">
-                                <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Conferma Password') }}</label>
-    
-                                <div class="col-md-6">
-                                    <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
-                                </div>
+                            <div class="input">
+                                
+                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
+
+                                <label for="password-confirm" class="label-input">{{ __('Conferma Password') }}</label>
+                                
                             </div>
     
-                            <div class="mb-4 row">
-                                <label for="restaurant_img" class="col-md-4 col-form-label text-md-right">Aggiungi immagine</label>
-                                <div class="col-md-6">
-                                    <input class="form-control" name="restaurant_img" type="file" id="restaurant_img">
-                                </div>
+                            <div class="input">
+                            
+                                <input id="address" type="text" class="form-control @error('address') is-invalid @enderror" name="address" value="{{ old('address') }}" required autocomplete="address">
+
+                                @error('address')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                                @enderror
+
+                                <label for="address" class="label-input">{{ __('Indirizzo') }}</label>
+                                
+                            </div>
+    
+                            <div class="input">
+                                
+                                <input id="vat_id" type="text" class="form-control @error('address') is-invalid @enderror" name="vat_id" value="{{ old('vat_id') }}" required autocomplete="vat_id">
+
+                                @error('vat_id')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                                @enderror
+
+                                <label for="vat_id" class="label-input">{{ __('Partita IVA') }}</label>
+                                
+                            </div>
+
+                            <div class="input d-flex">
+                                <label for="restaurant_img" class="label-input-file">Aggiungi immagine</label>
+                                
+                                    <input class="input-file" name="restaurant_img" type="file" id="restaurant_img">
+                               
                             </div>
     
                             <p>Seleziona la Tipologia</p>
@@ -80,44 +108,12 @@
                                 </div>
                                 @endforeach
                             </div>
-    
-                            <div class="mb-4 row">
-                                <label for="address" class="col-md-4 col-form-label text-md-right">{{ __('Indirizzo') }}</label>
-    
-                                <div class="col-md-6">
-                                    <input id="address" type="text" class="form-control @error('address') is-invalid @enderror" name="address" value="{{ old('address') }}" required autocomplete="address">
-    
-                                    @error('address')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                    @enderror
-                                </div>
-                            </div>
-    
-                            <div class="mb-4 row">
-                                <label for="vat_id" class="col-md-4 col-form-label text-md-right">{{ __('Partita IVA') }}</label>
-    
-                                <div class="col-md-6">
-                                    <input id="vat_id" type="text" class="form-control @error('address') is-invalid @enderror" name="vat_id" value="{{ old('vat_id') }}" required autocomplete="vat_id">
-    
-                                    @error('vat_id')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                    @enderror
-                                </div>
-                            </div>
-    
-    
-                            <div class="mb-4 row mb-0">
-                                <div class="col-md-6 offset-md-4">
-                                    <button type="submit" class="btn btn-primary">
-                                        {{ __('Register') }}
-                                    </button>
-                                </div>
-                            </div>
+                                    <button type="submit" class="btn btn-primary orange">
+                                        {{ __('Registra') }}
+                                    </button>                              
                         </form>
+
+                        <p class="info">Se sei già registrato <a class="form-link" href="{{ route('login') }}">clicca qui</a></p>
                         @if ($errors->any())
                   <div class="alert alert-danger">
                       <ul>
