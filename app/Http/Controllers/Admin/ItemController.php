72b22faacd8dc81e2e7671a63bde983a1a54b024
CarloVeronese <carlo.veronese97@gmail.com>
2024-02-01 10:29:21 +0100

    add - routes and controller

--- app/Http/Controllers/Admin/ItemController.php
+++ app/Http/Controllers/Admin/ItemController.php
@@ -6,62 +6,79 @@ use App\Http\Controllers\Controller;
 use App\Models\Item;
 use App\Http\Requests\StoreItemRequest;
 use App\Http\Requests\UpdateItemRequest;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Str;
 
 class ItemController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
-    public function index()
+    public function index(string $slug)
     {
-        //
+        $user_slug = Auth::user()->slug;
+        if ($slug ==  $user_slug) {
+            $user_id = Auth::user()->id;
+            $items = Item::where('user_id', '=', $user_id)->get();
+            return view('admin.items.index', compact('items'));
+        }
+        else {
+            return view('admin.errors.error');
+        }
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
+    public function create(string $slug)
     {
-        //
+        $user= Auth::user();
+        if ($slug ==  $user->slug) {
+            return view('admin.items.create', compact('user'));
+        }
+        else {
+            return view('admin.errors.error');
+        }
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
     public function store(StoreItemRequest $request)
     {
-        //
+        $user= Auth::user();
+        $data = $request->all();
+        $data['slug'] = Str::slug($data['name']);
+        $item = Item::create($data);
+        $item->user()->associate($user);
+        $item->save();
+        return redirect()->route('admin.items.show', ['slug' => $user->slug, 'item' => $item]);
     }
 
-    /**
-     * Display the specified resource.
-     */
-    public function show(Item $item)
+    public function show(string $slug, Item $item)
     {
-        //
+        $user= Auth::user();
+        if ($user->id ==  $item->user->id && $slug ==  $user->slug) {
+            return view('admin.items.show', compact('item', 'user'));
+        }
+        else {
+            return view('admin.errors.error');
+        }
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(Item $item)
+    public function edit(string $slug, Item $item)
     {
-        //
+        $user= Auth::user();
+        if ($user->id ==  $item->user->id && $slug ==  $user->slug) {
+            return view('admin.items.edit', ['item'=>$item, 'user'=>$user]);
+        }
+        else {
+            return view('admin.errors.error');
+        }
     }
 
-    /**
-     * Update the specified resource in storage.
-     */
     public function update(UpdateItemRequest $request, Item $item)
     {
-        //
+        $user= Auth::user();
+        $data = $request->all();
+        $data['slug'] = Str::slug($data['name']);
+        $item->update($data);
+        return redirect()->route('admin.items.show', ['slug' => $user->slug, 'item' => $item]);
     }
 
-    /**
-     * Remove the specified resource from storage.
-     */
     public function destroy(Item $item)
     {
-        //
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\Admin\ItemController;
+use App\Http\Controllers\Auth\RegisteredUserController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -22,10 +24,14 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
+Route::middleware(['auth', 'verified'])->prefix('{slug}')->name('admin.')->group(function () {
+    Route::resource('/items', ItemController::class);
+});
+
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-require __DIR__.'/auth.php';
+require __DIR__. '/auth.php';
