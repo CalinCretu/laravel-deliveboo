8f86736d9bce9a33dd7a34c435206ecd62f5f7b2
CalinCretu <calincretu1412@gmail.com>
2024-02-05 16:45:23 +0100

    FIX - show view buttons, responsive and header position

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -26,60 +26,64 @@
 
         <header>
             <div class="container">
-            <nav class="my-navbar">
-                <div class="logo">
-                    <a class="" href="{{ url('/') }}">
-                        <img class="d-none d-sm-none d-md-block d-lg-block d-xl-block " width="20%" class="item-img" src="{{Vite::asset('resources/img/Logo 3.png')}}" alt="">
-                           {{-- config('app.name', 'Laravel') --}}
-                        <img class="d-block d-md-none" width="15%" class="item-img" src="{{Vite::asset('resources/img/Logo 1.png')}}" alt="">
+                <nav class="my-navbar">
+                    <div class="logo">
+                        <a class="" href="{{ url('/') }}">
+                            <img class="d-none d-sm-none d-md-block d-lg-block d-xl-block " width="20%"
+                                class="item-img" src="{{ Vite::asset('resources/img/Logo 3.png') }}" alt="">
+                            {{-- config('app.name', 'Laravel') --}}
+                            <img class="d-block d-md-none" width="15%" class="item-img"
+                                src="{{ Vite::asset('resources/img/Logo 1.png') }}" alt="">
                         </a>
                     </div>
-    
+
                     {{-- <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     </button> --}}
-    
+
                     {{-- <div class="collapse navbar-collapse" id="navbarSupportedContent"> --}}
-                        <!-- Left Side Of Navbar -->
-                        {{-- <ul class="navbar-nav me-auto">
+                    <!-- Left Side Of Navbar -->
+                    {{-- <ul class="navbar-nav me-auto">
                             <li class="nav-item">
                                 <a class="nav-link" href="{{url('/') }}">{{ __('Home') }}</a>
                             </li>
                         </ul> --}}
-    
-                        <!-- Right Side Of Navbar -->
-                        <ul class="my-nav-list">
-                            <!-- Authentication Links -->
-                            @guest
+
+                    <!-- Right Side Of Navbar -->
+                    <ul class="my-nav-list">
+                        <!-- Authentication Links -->
+                        @guest
                             <li class="nav-item ">
                                 <a class="nav-link col-white" href="{{ route('login') }}">{{ __('Login') }}</a>
                             </li>
                             @if (Route::has('register'))
-                            <li class="nav-item">
-                                <a class="nav-link col-white" href="{{ route('register') }}">{{ __('Register') }}</a>
-                            </li>
+                                <li class="nav-item">
+                                    <a class="nav-link col-white" href="{{ route('register') }}">{{ __('Register') }}</a>
+                                </li>
                             @endif
-                            @else
+                        @else
                             <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"
+                                    data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                                     {{ Auth::user()->name }}
                                 </a>
-    
+
                                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
                                     @yield('dashboard')
                                     {{-- <a class="dropdown-item" href="{{route('dashboard', $user->slug)}}">{{__('Dashboard')}}</a> --}}
-                                    <a class="dropdown-item" href="{{ url('profile') }}">{{__('Profile')}}</a>
-                                    <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
+                                    <a class="dropdown-item" href="{{ url('profile') }}">{{ __('Profile') }}</a>
+                                    <a class="dropdown-item" href="{{ route('logout') }}"
+                                        onclick="event.preventDefault();
                                                          document.getElementById('logout-form').submit();">
                                         {{ __('Logout') }}
                                     </a>
-    
+
                                     <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
                                         @csrf
                                     </form>
                                 </div>
                             </li>
-                            @endguest
-                        </ul>
+                        @endguest
+                    </ul>
                     {{-- </div> --}}
                 </nav>
             </div>
